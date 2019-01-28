eac667cec267504fbe0bafdec6127c511d1c1c21
Eva <evaliisagalen@gmail.com>
2019-01-28 13:36:03 -0800

    moved api_key + request limit check up a level; added new route 'destinations'

--- routes/v1.rb
+++ routes/v1.rb
@@ -3,37 +3,30 @@ class App
 
   route 'v1' do |r|
     r.on 'welcome' do
-      r.get 'flights' do
-        api_key = r.params['api_key']
-        # check that params are present
-        if api_key #!= "" => check that there is an api key included
-          if ValidateApiKey.new(api_key).api_key_valid?
-            if RateLimiting.new.rate_limit_not_exceeded?(api_key)
+      api_key = r.params['api_key']
+      if api_key != "" # check that there is an api key included
+        if ValidateApiKey.new(api_key).api_key_valid?
+          if RateLimiting.new.rate_limit_not_exceeded?(api_key)
+      # move api key check + rate limit here
+            r.get 'flights' do
               #validate request params
               valid_params = LufthansaApiCalls.new(r.params).request_params_valid?
               #response = LufthansaApiCalls.new(r.params).get_flights
               #add error handling
-            else
-              r.halt(
-                429,
-                error: 'Too many requests',
-                message: ErrorMessages::LIMIT_EXCEEDED
-              )
             end
+
+            r.get 'airport' do
+              p("I selected #{ r.params['airport'] }")
+            end
+
           else
-            r.halt(
-              401,
-              error: 'Unauthorized',
-              message: ErrorMessages::API_KEY_INVALID
-            )
+            r.halt(429, message: ErrorMessages::LIMIT_EXCEEDED)
           end
         else
-          r.halt(
-            400,
-            error: 'Bad request',
-            message: ErrorMessages::QUERY_INVALID
-          )
+          r.halt(401, message: ErrorMessages::API_KEY_INVALID)
         end
+      else
+        r.halt(400, message: ErrorMessages::QUERY_INVALID)
       end
     end
   end
--- scraping/wikipedia.rb
+++ scraping/wikipedia.rb
@@ -1,19 +1,22 @@
 require 'nokogiri'
 require 'rest-client'
-
-wiki_page = Nokogiri::HTML(RestClient.get("https://en.wikipedia.org/wiki/San_Francisco_International_Airport"))
-table = wiki_page.css('table')[3]
+lax_url = "https://en.wikipedia.org/wiki/Los_Angeles_International_Airport"
+lax_table_number = 4
+sfo_url = "https://en.wikipedia.org/wiki/San_Francisco_International_Airport"
+sfo_table_number = 3
+sxm_url = "https://en.wikipedia.org/wiki/Princess_Juliana_International_Airport"
+sxm_table_number = 2
+wiki_page = Nokogiri::HTML(RestClient.get(sxm_url))
+table = wiki_page.css('table')[sxm_table_number] #replace idx with table number
 
 rows = table.css('tr')
 column_names = rows.shift.css('th').map(&:text)
 
 text_all_rows = rows.map do |row|
-
   row_values = row.css('td').map(&:text)
-
   [*row_values]
 end
 
 text_all_rows.each do |row_as_text|
-  column_names.zip(row_as_text).to_h
+  p column_names.zip(row_as_text).to_h
 end
