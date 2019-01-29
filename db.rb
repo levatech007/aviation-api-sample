a4705f259cf4a612010b43613a15f84a028305e0
Eva <evaliisagalen@gmail.com>
2019-01-28 17:27:50 -0800

    uncommented seed data

--- db.rb
+++ db.rb
@@ -14,28 +14,28 @@ DB = Sequel.connect(ENV.delete('APP_DATABASE_URL') || ENV.delete('DATABASE_URL')
 # get seed file from /seeds/seed.rb
 # clear DB entries before running the seeds or check if entries already exist.
 
-# airports = DB[:airports]
-# airports.delete # CAREFUL! DELETES ALL PREVIOUS ENTRIES
-# airports_list = [
-#   ["San Francisco International", "SFO", "KSFO", 37.615215, -122.38988, "America/Los_Angeles", -8, "United States", "US", "SFO"],
-#   ["Princess Juliana International", "SXM", "TNCM", 18.044722, -63.11406, "America/Lower_Princes", -4, "Sint Maarten", "SX", "SXM"],
-#   ["Los Angeles International", "LAX", "KLAX", 33.943398, -118.40828, "America/Los_Angeles", -8, "United States", "US", "LAX"]
-#  ]
-#    airports_list.each do |airport|
-#      airports.insert(airport_name: airport[0], airport_iata_code: airport[1], airport_icao_code: airport[2], latitude: airport[3], longitude: airport[4], timezone: airport[5], gmt: airport[6], country_name: airport[7], iso2_country_code: airport[8], iata_city_code: airport[9])
-#    end
+airports = DB[:airports]
+airports.delete # CAREFUL! DELETES ALL PREVIOUS ENTRIES
+airports_list = [
+  ["San Francisco International", "SFO", "KSFO", 37.615215, -122.38988, "America/Los_Angeles", -8, "United States", "US", "SFO"],
+  ["Princess Juliana International", "SXM", "TNCM", 18.044722, -63.11406, "America/Lower_Princes", -4, "Sint Maarten", "SX", "SXM"],
+  ["Los Angeles International", "LAX", "KLAX", 33.943398, -118.40828, "America/Los_Angeles", -8, "United States", "US", "LAX"]
+ ]
+   airports_list.each do |airport|
+     airports.insert(airport_name: airport[0], airport_iata_code: airport[1], airport_icao_code: airport[2], latitude: airport[3], longitude: airport[4], timezone: airport[5], gmt: airport[6], country_name: airport[7], iso2_country_code: airport[8], iata_city_code: airport[9])
+   end
 
 
 # adds destinations to each airport:
 # airports = DB[:airports]
-# response = RestClient.get(ENV['APPS_SCRIPT_URL'])
-# destinations_data = JSON.parse(response)
-# destinations_data.each do |airport_data|
-#   p airport_data['airportIataCode']
-#   all_destinations = []
-#   airport_data['data'].each do |airline|
-#     all_destinations << airline['destinations']
-#   end
-#   unique_destinations = all_destinations.join(",").split(",").uniq.join(",")
-#   airports.where(airport_iata_code: airport_data['airportIataCode']).update(destinations: unique_destinations)
-# end
+response = RestClient.get(ENV['APPS_SCRIPT_URL'])
+destinations_data = JSON.parse(response)
+destinations_data.each do |airport_data|
+  p airport_data['airportIataCode']
+  all_destinations = []
+  airport_data['data'].each do |airline|
+    all_destinations << airline['destinations']
+  end
+  unique_destinations = all_destinations.join(",").split(",").uniq.join(",")
+  airports.where(airport_iata_code: airport_data['airportIataCode']).update(destinations: unique_destinations)
+end
