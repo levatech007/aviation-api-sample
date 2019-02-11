0f93388588b040197342531829e7d1e10a2afc7f
Eva <evaliisagalen@gmail.com>
2019-02-11 15:52:20 -0800

    finalized aircraft seed functions

--- seeds/seed.rb
+++ seeds/seed.rb
@@ -1,6 +1,5 @@
 class SeedDatabase
   require 'json'
-  require 'sequel/core'
   require 'rest-client'
 
   def get_data()
@@ -17,17 +16,33 @@ class SeedDatabase
   # end
 
   def seed_aircraft_database
+    # 2 separated sources for aircraft data:
+
+    # get data from apps script sheet:
     aircraft_data = get_data()
     aircraft_data['data'].map do |aircraft|
-      p(aircraft)
       Aircraft.insert(
         iata_aircraft_code:  aircraft['iata_code'],
         icao_aircraft_code:  aircraft['icao_code'],
-        aircraft_name:       aircraft['aircraft_model'],
+        aircraft_name:       aircraft['aircraft_model'].gsub!("\n","") , # remove newline!
         wake_category:       "n/a"
                    )
-
+    end
+    # add in data from a json file:
+    file = File.read('./seeds/formatted_aircrafts_data.json')
+    data = JSON.parse(file)
+    aircraft_list = data['aircrafts']
+    aircraft_list.map do |aircraft|
+      aircraft_json = JSON.parse(aircraft)
+      if !Aircraft.find(iata_aircraft_code: aircraft_json['iata_aircraft_code'])
+          Aircraft.insert(
+                          iata_aircraft_code:  aircraft_json['iata_aircraft_code'],
+                          icao_aircraft_code:  aircraft_json['icao_aircraft_code'],
+                          aircraft_name:       aircraft_json['aircraft_name'],
+                          wake_category:       aircraft_json['wake_category']
+                        )
+      # else: if Aircraft is found and wake_category is "n/a", add wake category
+      end
     end
   end
-
 end
