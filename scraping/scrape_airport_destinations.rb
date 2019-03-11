f46d691c8655e942e4c6b6423b2d93001ef3d280
Eva <evaliisagalen@gmail.com>
2019-03-11 14:06:49 -0700

    adjusted destinations scraping to include possible variations in wiki tables

--- scraping/scrape_airport_destinations.rb
+++ scraping/scrape_airport_destinations.rb
@@ -11,49 +11,60 @@
 
   #def scrape_destinations
 
-
-    example_url = "https://en.wikipedia.org/wiki/San_Francisco_International_Airport"
+    example_url = "https://en.wikipedia.org/wiki/Narita_International_Airport"
     # url = "#{ BASE_URL }#{ @airport_wiki_page }"
     html = RestClient.get(example_url)
     # html = RestClient.get(BASE_URL)
     wiki_page = Nokogiri::HTML(html)
-    tables = wiki_page.css('table')
-    result = []
+    tables    = wiki_page.css('table')
+    result = {}
+    airlines_destinations_table_found = false
+
     tables.each do |table|
-      rows = table.css('tr')
+      rows     = table.css('tr')
       row_name = rows.css('th').text
-      if row_name == "AirlinesDestinationsRefs" # => there is no unique id for table, find it by <th> values
-      #   p("Selected row: " + row_name)
-        all_rows_text = rows.map do |row|
-          row.css('td').map(&:text)
-        end
-        p(*all_rows_text)
-        all_rows_text.each do |row|
-          if !row.empty?
-            result.push(row[1].gsub(/\s+/, "")) # row[1] is destinations column, remove all whitespace
+
+      if !airlines_destinations_table_found
+        if row_name == 'AirlinesDestinationsRefs' || row_name == 'AirlinesDestinations'  # => there is no unique id for table, find it by <th> value "AirlinesDestinationsRefs", contains necessary data
+          airlines_destinations_table_found = true # destinations table name 'AirlinesDestinations' applies to passenger and cargo tables. First is always passenger traffic
+          all_destination_row_text  = rows.map { |row| row.css('td').map(&:text) }
+          all_destination_row_links = rows.map { |row| row.css('td a').map { |link| link['href'] } }
+
+          all_destinations = []
+          all_destination_row_text.map do |row|
+            all_destinations.push(row[1].gsub(/\s+/, "")) if !row.empty? # row[1] is destinations column, remove all whitespace
           end
+
+          all_wiki_links = []
+          all_destination_row_links.map { |row| all_wiki_links.push(row[1...-1]) } # take all values except first (airline link)
+
+          result = {
+                      all_destinations: all_destinations,
+                      wiki_pages: all_wiki_links
+                   }
         end
-        p(result)
-        # format_scraped_data(result)
       end
-
     end
+    #return result
+    p(result)
 
-    def format_scraped_data
-      update_dates = []
-      regular_destinations = []
-      seasonal_destinations = []
-      # for each destinations array:
-      # split to individual destinations
-      # remove refs ex. [123]
-      # get dates from (ending March 31, 2019), (begins ...), (resumes ...)
-      # remove destinations with (begins ...) or (resumes ...) => future destinations need to be stored separately?
-      #keep track of schedule changes with update_dates
-      # return hash { update_dates: [], regular_destinations: [].uniq, seasonal_destinations: [] }
-    end
+    # def format_destinations(data_array)
+    #   regular_destinations = []
+    #   seasonal_destinations = []
+    #   # for each destinations array:
+    #   # split to individual destinations
+    #   # remove refs ex. [123]
+    #   # remove destinations with (begins...) (resumes...) for now
+    #   # keep destination
+    #   # remove destinations with (begins ...) or (resumes ...) => future destinations need to be stored separately?
+    #   # return hash { regular_destinations: [], seasonal_destinations: [] }
+    # end
     #
-    # return only unique destinations
-
+    # def format_wiki_links(links)
+    #   # links is array of arrays
+    #   # remove nil or []
+    #   #
+    # end
 
 
   #end
