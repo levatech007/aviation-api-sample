85119f342975d622d5a3f1dc16be31a6d1f6f384
Eva <evaliisagalen@gmail.com>
2019-03-18 10:29:18 -0700

    added initialmethod to clean and format destinations data

--- scraping/scrape_airport_destinations.rb
+++ scraping/scrape_airport_destinations.rb
@@ -10,8 +10,8 @@
   # end
 
   #def scrape_destination
-
-    example_url = "https://en.wikipedia.org/wiki/Zurich_Airport"
+    # Zurich_Airport
+    example_url = "https://en.wikipedia.org/wiki/San_Francisco_International_Airport"
     # url = "#{ BASE_URL }#{ @airport_wiki_page }"
     html = RestClient.get(example_url)
     # html = RestClient.get(BASE_URL)
@@ -32,22 +32,54 @@
           # First is always passenger traffic. Use T/F to make sure only first of the two tables is returned.
           airlines_destinations_table_found = true
           all_destinations = rows.map do |row|
-            all_destinations = row.css('td').map do |td|
+            td = row.css('td')[1]
+              if td
               destinations = td.text.delete(" \n").split(",")
-
               links = td.css('a').map {|link| link['href'] }
+              # also get all value attributes?
+              # format destinations strings
+              # cleaned_destinations = .....
               # remove unnecessary links
               cleaned_links = links.reject { |link| link.include?('#cite_note') }
-              p([destinations,cleaned_links])
+              #p(destinations.zip(cleaned_links))
             end
           end
-
+          #p(all_destinations)
         end
       end
     end
 
+    def clean_destinations(arr)
+      result = []
+      seasonal_exists = false
+      add_seasonal = false
+      add_seasonal_charter = false
+      # initial working function, needs refactoring...not DRY
+      arr.each_with_index do |item, idx|
 
+        if add_seasonal_charter
+          result << "Seasonalcharter:" + item
+        elsif add_seasonal
+          result << "Seasonal:" + item
+        elsif item.include?("Seasonalcharter:")
+          add_seasonal_charter = true
+          add_seasonal = false
+          split_str = item.split("Seasonalcharter:")
+          result << "Seasonal:" + split_str.delete_at(0)
+          result << "Seasonalcharter:" + split_str.delete_at(0)
+        elsif item.include?("Seasonal:")
+          add_seasonal = true
+          split_str = item.split("Seasonal:")
+          result << split_str.delete_at(0)
+          result << "Seasonal:" + split_str.delete_at(0)
+        else
+          result << item
+        end
+         add_seasonal = false if arr[idx + 1] && arr[idx + 1].include?("Seasonalcharter:")
 
+      end
+      result
+    end
 
 
     def format_destinations(data_array)
