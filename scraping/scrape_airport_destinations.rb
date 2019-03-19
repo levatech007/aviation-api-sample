ac4e4f7d75dfa73a7cbcf7ad47693eb240d17629
Eva <evaliisagalen@gmail.com>
2019-03-19 09:55:28 -0700

    completed initial destinations scraping methods; minor revisions still needed

--- scraping/scrape_airport_destinations.rb
+++ scraping/scrape_airport_destinations.rb
@@ -4,12 +4,46 @@
   require 'rest-client'
 
   # BASE_URL = "https://en.wikipedia.org"
-
   # def initialize(airport_wiki_page)
   #   @airport_wiki_page = airport_wiki_page
   # end
 
-  #def scrape_destination
+  def clean_destinations(arr)
+  result = []
+  seasonal_exists = false
+  add_seasonal = false
+  add_seasonal_charter = false
+  arr.each_with_index do |item, idx|
+    # still need to remove refs [123] form item
+    if add_seasonal_charter
+      result << "Seasonalcharter:" + item
+    elsif add_seasonal
+      result << "Seasonal:" + item
+    elsif item.include?("Seasonalcharter:")
+      add_seasonal_charter = true
+      add_seasonal = false
+      split_str = item.split("Seasonalcharter:")
+      first_item = split_str.delete_at(0)
+      result << "Seasonal:" + first_item if first_item != ""
+      result << "Seasonalcharter:" + split_str.delete_at(0)
+    elsif item.include?("Seasonal:")
+      add_seasonal = true
+      split_str = item.split("Seasonal:")
+      first_item = split_str.delete_at(0)
+      result << first_item if first_item != ""
+      result << "Seasonal:" + split_str.delete_at(0)
+    else
+      result << item
+    end
+     add_seasonal = false if arr[idx + 1] && arr[idx + 1].include?("Seasonalcharter:")
+
+  end
+  result
+end
+
+
+
+  #def scrape_destinations
     # Zurich_Airport
     example_url = "https://en.wikipedia.org/wiki/San_Francisco_International_Airport"
     # url = "#{ BASE_URL }#{ @airport_wiki_page }"
@@ -34,75 +68,27 @@
           all_destinations = rows.map do |row|
             td = row.css('td')[1]
               if td
-              destinations = td.text.delete(" \n").split(",")
-              links = td.css('a').map {|link| link['href'] }
-              # also get all value attributes?
-              # format destinations strings
-              # cleaned_destinations = .....
-              # remove unnecessary links
-              cleaned_links = links.reject { |link| link.include?('#cite_note') }
-              #p(destinations.zip(cleaned_links))
+                destinations = td.text.delete(" \n").split(/\,(?![\d])/) # there are commas between dates that should not be split at! (beginsApril15,2019)
+                links = td.css('a').map {|link| link['href'] }
+                cleaned_destinations = clean_destinations(destinations)
+                cleaned_links = links.reject { |link| link.include?('#cite_note') }
+                cleaned_destinations.zip(cleaned_links)
             end
           end
-          #p(all_destinations)
+          p(all_destinations)
+          all_destinations
         end
       end
     end
-
-    def clean_destinations(arr)
-      result = []
-      seasonal_exists = false
-      add_seasonal = false
-      add_seasonal_charter = false
-      # initial working function, needs refactoring...not DRY
-      arr.each_with_index do |item, idx|
-
-        if add_seasonal_charter
-          result << "Seasonalcharter:" + item
-        elsif add_seasonal
-          result << "Seasonal:" + item
-        elsif item.include?("Seasonalcharter:")
-          add_seasonal_charter = true
-          add_seasonal = false
-          split_str = item.split("Seasonalcharter:")
-          result << "Seasonal:" + split_str.delete_at(0)
-          result << "Seasonalcharter:" + split_str.delete_at(0)
-        elsif item.include?("Seasonal:")
-          add_seasonal = true
-          split_str = item.split("Seasonal:")
-          result << split_str.delete_at(0)
-          result << "Seasonal:" + split_str.delete_at(0)
-        else
-          result << item
-        end
-         add_seasonal = false if arr[idx + 1] && arr[idx + 1].include?("Seasonalcharter:")
-
-      end
-      result
-    end
-
 
     def format_destinations(data_array)
       regular_destinations = []
       seasonal_destinations = []
       charter_destinations = []
-      # split string initially by:
-      delimiters = ['Seasonal:', 'Seasonalcharter:']
-      all_destinations = str.split(Regexp.union(delimiters))
-
-      # for each destinations array:
-      # split to individual destinations
-      # remove refs ex. [123]
-      # remove destinations with (begins...) (resumes...) for now
-      # keep destination
+      
+      # keep destination (ends ...)
       # remove destinations with (begins ...) or (resumes ...) => future destinations need to be stored separately?
-      # return hash { regular_destinations: [], seasonal_destinations: [] }
-    end
-
-    def format_wiki_links(links)
-      # links is array of arrays
-      # remove nil or []
-      #
+      # return hash { airport_iata_code: "XXX", airport_wiki_page: "/wiki/xxxxxxxx", regular_destinations: [], seasonal_destinations: [], charter_destinations: [] }
     end
 
 
