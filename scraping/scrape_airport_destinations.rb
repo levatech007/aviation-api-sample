a0873554e1ca01fe80a6cbfccdcf53164ab2afcc
Eva <evaliisagalen@gmail.com>
2019-03-25 10:14:14 -0700

    turned scrape_airport_destinations.rb into a Class

--- scraping/scrape_airport_destinations.rb
+++ scraping/scrape_airport_destinations.rb
@@ -1,12 +1,50 @@
 # scrape airport destinations
-#class ScrapeAirportDestinations
+class ScrapeAirportDestinations
   require 'nokogiri'
   require 'rest-client'
 
-  # BASE_URL = "https://en.wikipedia.org"
-  # def initialize(airport_wiki_page)
-  #   @airport_wiki_page = airport_wiki_page
-  # end
+  BASE_URL = "https://en.wikipedia.org"
+
+  def initialize(airport_wiki_page)
+    @airport_wiki_page = airport_wiki_page
+  end
+
+  def scrape_destinations
+      url = "#{ BASE_URL }#{ @airport_wiki_page }"
+      html = RestClient.get(url)
+      wiki_page = Nokogiri::HTML(html)
+      tables = wiki_page.css('table')
+
+      airlines_destinations_table_found = false
+
+      tables.each do |table|
+        rows     = table.css('tr')
+        row_name = rows.css('th').text
+
+        if !airlines_destinations_table_found
+          # there is no unique id for destinations table
+          # find it by <th> value "AirlinesDestinationsRefs" or first instace of "AirlinesDestinations" => contains necessary data
+          if row_name == 'AirlinesDestinationsRefs' || row_name == 'AirlinesDestinations'
+          p("Table:")
+            p(table)
+            # destinations table name 'AirlinesDestinations' applies to both passenger and cargo tables.
+            # First is always passenger traffic. Use T/F to make sure only first of the two tables is returned.
+            airlines_destinations_table_found = true
+            all_destinations = rows.map do |row|
+              td = row.css('td')[1]
+                if td
+                  destinations = td.text.delete(" \n").split(/\,(?![\d])/) # there are commas between dates that should not be split at! (beginsApril15,2019)
+                  links = td.css('a').map {|link| link['href'] }
+                  cleaned_destinations = clean_destinations(destinations)
+                  cleaned_links = links.reject { |link| link.include?('#cite_note') }
+                  cleaned_destinations.zip(cleaned_links)
+              end
+            end
+            return format_destinations(all_destinations.compact)
+          end
+        end
+      end
+    end
 
   def clean_destinations(arr)
   # function works but needs refactoring, not DRY & needs better variable names
@@ -52,83 +90,39 @@
     result
   end
 
-# clean destinations strings that includes routes with 'begins', 'resumes' or 'ends' dates
-# "London(beginsApril15,2020)" => ["London", "begins", "April5,2020"]
-def format_transitional_destinations(dest_str)
-  result = []
-  types  = ['begins', 'ends', 'resumes']
-  match  = types.find { |type| dest_str.include?(type) }
-  result_arr = dest_str.split("(")
-  date = result_arr[1].gsub(/#{ match }|\)/, '') # remove the type and remaining ')'
-  result.push(result_arr[0], match, date)
-  result
-end
-
-def format_destinations(data_array)
-# => each destination type array contains destinations objects [{}, {}, {}]
-# return hash with separated destinations
-  destinations = {
-    regular: [],
-    seasonal: [],
-    seasonalcharter: []
-  }
-  # loop through destinations data
-  data_array.map do |single_airline_destinations|
-    single_airline_destinations.map do |destination|
-      destination_types = ["Seasonalcharter", "Seasonal"]
-      current_destination = destination[0]
-      match = destination_types.find { |type| current_destination.include?(type) } || 'regular'
-      if current_destination.match(/begins|resumes|ends/)
-        current_destination = format_transitional_destinations(current_destination)
-      end
-      destinations[match.downcase.to_sym].push({ destination: current_destination, wiki_page: destination[1] })
-    end
+  # clean destinations strings that includes routes with 'begins', 'resumes' or 'ends' dates
+  # "London(beginsApril15,2020)" => ["London", "begins", "April5,2020"]
+  def format_transitional_destinations(dest_str)
+    result = []
+    types  = ['begins', 'ends', 'resumes']
+    match  = types.find { |type| dest_str.include?(type) }
+    result_arr = dest_str.split("(")
+    date = result_arr[1].gsub(/#{ match }|\)/, '') # remove the type and remaining ')'
+    result.push(result_arr[0], match, date)
+    result
   end
-  p(destinations)
-  return destinations
-end
 
-  #def scrape_destinations
-    # Zurich_Airport
-    example_url = "https://en.wikipedia.org/wiki/Tallinn_Airport"
-    # url = "#{ BASE_URL }#{ @airport_wiki_page }"
-    html = RestClient.get(example_url)
-    # html = RestClient.get(BASE_URL)
-    wiki_page = Nokogiri::HTML(html)
-    tables    = wiki_page.css('table')
-
-    airlines_destinations_table_found = false
-
-    tables.each do |table|
-      rows     = table.css('tr')
-      row_name = rows.css('th').text
-
-      if !airlines_destinations_table_found
-        # there is no unique id for destinations table
-        # find it by <th> value "AirlinesDestinationsRefs" or first instace of "AirlinesDestinations" => contains necessary data
-        if row_name == 'AirlinesDestinationsRefs' || row_name == 'AirlinesDestinations'
-          # destinations table name 'AirlinesDestinations' applies to both passenger and cargo tables.
-          # First is always passenger traffic. Use T/F to make sure only first of the two tables is returned.
-          airlines_destinations_table_found = true
-          all_destinations = rows.map do |row|
-            td = row.css('td')[1]
-              if td
-                destinations = td.text.delete(" \n").split(/\,(?![\d])/) # there are commas between dates that should not be split at! (beginsApril15,2019)
-                links = td.css('a').map {|link| link['href'] }
-                cleaned_destinations = clean_destinations(destinations)
-                cleaned_links = links.reject { |link| link.include?('#cite_note') }
-                cleaned_destinations.zip(cleaned_links)
-            end
-          end
-          p(format_destinations(all_destinations.compact))
-
+  def format_destinations(data_array)
+  # => each destination type array contains destinations objects [{}, {}, {}]
+  # return hash with separated destinations
+    destinations = {
+      regular: [],
+      seasonal: [],
+      seasonalcharter: []
+    }
+    # loop through destinations data
+    data_array.map do |single_airline_destinations|
+      single_airline_destinations.map do |destination|
+        destination_types = ["Seasonalcharter", "Seasonal"]
+        current_destination = destination[0]
+        match = destination_types.find { |type| current_destination.include?(type) } || 'regular'
+        if current_destination.match(/begins|resumes|ends/)
+          current_destination = format_transitional_destinations(current_destination)
         end
+        destinations[match.downcase.to_sym].push({ destination: current_destination, wiki_page: destination[1] })
       end
     end
+    return destinations
+  end
 
-
-
-
-  #end
-
-#end
+end
