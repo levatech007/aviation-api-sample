141940ed3bdee8aa7387e5404026ac7c3f64290c
Eva <evaliisagalen@gmail.com>
2019-05-20 11:21:22 -0700

    save LH token to Redis, retrieve with each request

--- lib/gettoken.rb
+++ lib/gettoken.rb
@@ -1,5 +1,7 @@
 # request needed tokens for 3rd party API calls
 class GetToken
+  require 'redis'
+  require 'date'
   require 'rest-client'
   LH_TOKEN_URL = 'https://api.lufthansa.com/v1/oauth/token'.freeze
 
@@ -16,6 +18,17 @@ class GetToken
                                             payload:  data,
                                             headers:  { content_type: 'application/x-www-form-urlencoded' }
                                           )
-    JSON.parse(response)
+    lh_token = response.to_s
+    # key format for Redis: lhtoken:week00:day0 => week is week number, day is 1-7 (mon-sun) calendar day
+    # set to expire in 48 hrs (keep one as backup)
+    # store token in redis
+    redis = Redis.new
+    today = Date.today
+    key = "lhtoken:week#{ today.cweek }:day#{ today.cwday }"
+    expire = 60 * 60 * 48
+    unless redis.exists(key)
+      redis.set(key, lh_token)
+      redis.expire(key, expire)
+    end
   end
 end
--- lib/lufthansaapicalls.rb
+++ lib/lufthansaapicalls.rb
@@ -9,17 +9,17 @@ class LufthansaApiCalls
   LH_BASE_URL = 'https://api.lufthansa.com/v1/operations/schedules/'
 
   def initialize(request_params)
-    @from    = request_params['from']
-    @to      = request_params['to']
-    @date    = request_params['date']
-    @nonstop = request_params['nonstop'] # should default to 0
+    @from    = request_params[:from].upcase
+    @to      = request_params[:to].upcase
+    @date    = request_params[:date]
+    @nonstop = request_params[:nonstop] # should default to 0
   end
 
   def validate_request_params
     messages = []
     # validate airport codes
-    messages.push(ErrorMessages::DEPARTURE_INVALID) if !Airport.find(airport_iata_code: @from.upcase)
-    messages.push(ErrorMessages::ARRIVAL_INVALID)   if !Airport.find(airport_iata_code: @to.upcase)
+    messages.push(ErrorMessages::DEPARTURE_INVALID) if !Airport.find(airport_iata_code: @from)
+    messages.push(ErrorMessages::ARRIVAL_INVALID)   if !Airport.find(airport_iata_code: @to)
 
     # check if direct_flights is present
     if @nonstop != 0 || @nonstop != 1
@@ -27,7 +27,6 @@ class LufthansaApiCalls
     end
     # if present, it can be 1 or 0
     # if not present, default to 0,
-    p("Param for direct flights: #{ @nonstop }")
 
     if @date.length == 10
       begin
@@ -51,9 +50,15 @@ class LufthansaApiCalls
   end
 
   def get_flights
+    # get current token from Redis by key (ex. lhtoken:week21:day5)
     # Auth format LH expects: "Bearer kfu894usdbj"
-    lh_token = GetToken.new.obtain_lh_token
-    auth     = "#{lh_token['token_type'].capitalize} #{lh_token['access_token']}"
+    today = Date.today
+    redis = Redis.new
+    redis_key = "lhtoken:week#{today.cweek}:day#{today.cwday}"
+    get_lh_token = redis.get(redis_key)
+    # convert to hash
+    lh_token = JSON.parse(get_lh_token, symbolize_names: true)
+    auth     = "#{lh_token[:token_type].capitalize} #{lh_token[:access_token]}"
     # expected (example) url: LH_BASE_URL/SFO/FRA/2019-06-01?directFlights=1
     url      = LH_BASE_URL + "#{ @from }/#{ @to }/#{ @date }?directFlights=#{ @nonstop }"
     RestClient::Request.execute(
@@ -62,8 +67,8 @@ class LufthansaApiCalls
                                   headers:  { Authorization: auth, Accept: 'application/json' }
                                 ) { |response|
                                   # add handling if there are no flights to return!
-                                  data          = JSON.parse(response)
-                                  flights_data  = data['ScheduleResource']['Schedule']
+                                  data          = JSON.parse(response, symbolize_names: true)
+                                  flights_data  = data[:ScheduleResource][:Schedule]
                                   response.code == 200 ? format_get_flights_data(flights_data) : { error: "Something went wrong with LH" }
                                 }
   end
@@ -83,17 +88,17 @@ class LufthansaApiCalls
   def format_one_journey(journey)
     # journey['Flight'] is an object if the entire journey is a single non-stop flight,
     # otherwise it is an array of objects where each objects contains a single leg flight of the journey
-    if journey['Flight'].kind_of?(Array)
-      journey_flights, journey_stops   = [], journey['Flight'].length - 1
-      journey['Flight'].each { |flight| journey_flights.push(format_single_flight(flight))}
+    if journey[:Flight].kind_of?(Array)
+      journey_flights, journey_stops   = [], journey[:Flight].length - 1
+      journey[:Flight].each { |flight| journey_flights.push(format_single_flight(flight))}
     else
-      journey_flights, journey_stops   = format_single_flight(journey['Flight']), 0
+      journey_flights, journey_stops   = format_single_flight(journey[:Flight]), 0
     end
 
     one_formatted_journey = {
                                #route:             "FRA to SFO via MUC", # template string of all airports
                                stops:             journey_stops,
-                               journey_duration:  journey['TotalJourney']['Duration'].gsub(/[PT]/, ""), # LH string: "P[n]NDT[n]NH[n]NM"
+                               journey_duration:  journey[:TotalJourney][:Duration].gsub(/[PT]/, ""), # LH string: "P[n]NDT[n]NH[n]NM"
                                flights:           journey_flights
                             }
     # handle errors
@@ -103,17 +108,17 @@ class LufthansaApiCalls
   def format_single_flight(flight)
     # all flights have 'MarketingCarrier' that is LH, LH own flights ONLY have 'MarketingCarrier'
     # if operated by airline other than LH, 'OperatingCarrier' is included and should be used
-    carrier = flight.key?('OperatingCarrier') ?  'OperatingCarrier' : 'MarketingCarrier'
+    carrier = flight.key?(:OperatingCarrier) ?  'OperatingCarrier' : 'MarketingCarrier'
     one_formatted_flight = {
-                              flight_number:          "#{flight[carrier]['AirlineID']}#{flight[carrier]['FlightNumber']}",
-                              operated_by:            Airline.where(airline_iata_code: flight[carrier]['AirlineID']).get(:airline_name),
-                              departure_airport:      flight['Departure']['AirportCode'],
-                              departure_airport_name: Airport.where(airport_iata_code: flight['Departure']['AirportCode']).get(:airport_name),
-                              departure_time_local:   flight['Departure']['ScheduledTimeLocal']['DateTime'],
-                              arrival_airport:        flight['Arrival']['AirportCode'],
-                              arrival_airport_name:   Airport.where(airport_iata_code: flight['Arrival']['AirportCode']).get(:airport_name),
-                              arrival_time_local:     flight['Arrival']['ScheduledTimeLocal']['DateTime'],
-                              aircraft_type:          Aircraft.where(iata_aircraft_code: flight['Equipment']['AircraftCode'].to_s).get(:aircraft_name)
+                              flight_number:          "#{flight[carrier][:AirlineID]}#{flight[carrier][:FlightNumber]}",
+                              operated_by:            Airline.where(airline_iata_code: flight[carrier][:AirlineID]).get(:airline_name),
+                              departure_airport:      flight[:Departure][:AirportCode],
+                              departure_airport_name: Airport.where(airport_iata_code: flight[:Departure][:AirportCode]).get(:airport_name),
+                              departure_time_local:   flight[:Departure][:ScheduledTimeLocal][:DateTime],
+                              arrival_airport:        flight[:Arrival][:AirportCode],
+                              arrival_airport_name:   Airport.where(airport_iata_code: flight[:Arrival][:AirportCode]).get(:airport_name),
+                              arrival_time_local:     flight[:Arrival][:ScheduledTimeLocal][:DateTime],
+                              aircraft_type:          Aircraft.where(iata_aircraft_code: flight[:Equipment][:AircraftCode].to_s).get(:aircraft_name)
                           }
     # handle errors
     return one_formatted_flight
