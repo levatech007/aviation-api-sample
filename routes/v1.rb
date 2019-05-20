845aad3403e5fa0430ac5d80b78df89bf0b15cec
Eva <evaliisagalen@gmail.com>
2019-05-20 11:25:19 -0700

    update to route

--- routes/v1.rb
+++ routes/v1.rb
@@ -10,13 +10,21 @@ class App
           if RateLimiting.new.rate_limit_not_exceeded?(api_key)
             # begin routes
             r.get 'flights' do
-              request = LufthansaApiCalls.new(r.params).validate_request_params
-              request[:error] ? r.halt(400, errors: request[:messages]) : LufthansaApiCalls.new(r.params).get_flights
+              params_hash = Hash[r.params.map{|(k,v)| [k.to_sym,v]}] # keys need to be converted to symbols for Validations to work!
+              request = LufthansaApiCalls.new(params_hash).validate_request_params
+              request[:error] ? r.halt(400, errors: request[:messages]) : LufthansaApiCalls.new(params_hash).get_flights
             end
 
             r.get 'destinations' do
-              #get all selected airport destinations
-              Airports.new(r.params['airport']).get_airport_destinations
+              airport = r.params['airport']
+              unless airport.nil? || airport.empty?
+                # validate airport code
+                # get all selected airport destinations
+                validations = ParamValidations.new(r.params).validate_request_params
+                request[:error] ? r.halt(400, errors: request[:messages]) : Airports.new(r.params['airport']).get_airport_non_stop_destinations
+              else
+                r.halt(400, message: ErrorMessages::MISSING_PARAMS )
+              end
             end
 
           else
