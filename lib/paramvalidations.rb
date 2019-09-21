e2f3f92c38d9bd9d3b403bcc9b87b18dda9c1920
Eva <evaliisagalen@gmail.com>
2019-09-21 12:30:13 -0700

    moved some validations to DataValidation class; reconfigure validation flow

--- lib/paramvalidations.rb
+++ lib/paramvalidations.rb
@@ -1,68 +1,77 @@
 class ParamValidations
   require 'date'
-  @@errors = false
-  @@error_messages = []
 
   def validate_destinations_request_params(airport_code)
     @airport = airport_code
     # validate apirport code
+    DataValidations.new.valid_airport_iata_code?(@airport)
   end
 
   def validate_flight_request_params(request_params)
-    @from = request_params[:from]
-    @to = request_params[:from]
-    @date = request_params[:from]
-    @nonstop = request_params[:from]
-    # check if required params are present
-    # if not, return error stating what params are missing
-    # else validate params
-    # return has an array of messages that is empty if no errirs were found
-    validate_date
-    [@from, @to].each { |airport| validate_airport_iata_code(airport) }
+    @request_params  = request_params
+    @date            = request_params[:date]
+    @nonstop         = request_params[:nonstop]
+    @@airport_params = %i[from to]
+    @missing_params  = []
+    @error_messages  = []
+      # validate all and return messages:
+    # check that date is present, if yes, validate date, else add key to missing_params array
+    # validate date, add error message if invalid date
+    # check that both from, to keys are present; if key is not present, add to missing_params
+    # if airport key is present, validate airport code
+    # if there are no errors, nonstop selection will be checked
+    validate_request_date
+    validate_airport_params
     validate_nonstop_selection
+    format_validation_response
   end
 
-  def required_params_present?
-    
-  end
-
-  # validate airport code
-  def validate_airport_iata_code(airport_code)
-    # convert to upcase
-    Airport.find(airport_iata_code: airport_code)
-  end
-
-  def validate_date
-    error_messages = []
-    valid_date_pattern = /[0-9]{4}-[0-9]{2}-[0-9]{2}/ # YYYY-MM-DD
-    today = Date.today
-    max_date = today + 365
-    # Check for valid date pattern (YYYY-MM-DD) first as
-    # month, day without leading zeros  ex: "2018-9-5" is
-    # considered valid in Date.strptime.
-    # but API needs "2018-09-05" to process
-    # external API calls.
-    if valid_date_pattern.match?(@date)
-      begin
-        converted_date = Date.strptime(@date, '%Y-%m-%d')
-        unless converted_date.between?(today, max_date)
-          error_messages.push(ErrorMessages::INVALID_DATE_RANGE )
-        end
-      rescue ArgumentError => error
-          error_messages.push(error.message.capitalize)
-      end
+  def validate_request_date
+    if @request_params.key?(:date)
+      date_validation = DataValidations.new.validate_date(@date)
+      @error_messages.push(date_validation[:error_messages]) if date_validation[:errors]
     else
-      error_messages.push(ErrorMessages::INVALID_DATE_FORMAT)
+      @missing_params.push(:date)
     end
-    return error_messages
+  end
+
+  def validate_airport_params
+    @@airport_params.each do |key|
+      if @request_params.key? key
+        airport_code = @request_params[key]
+        unless DataValidations.new.valid_airport_iata_code?(airport_code)
+          @error_messages.push("#{ErrorMessages::INVALID_AIRPORT_CODE}#{ key.to_s }")
+        end
+      else
+        @missing_params.push(key.to_s)
+      end
+    end
   end
 
   def validate_nonstop_selection
+    # process only if there are no errors or missing params
     # check if nonstop is 1 or 0
-    # set default of 0
+    # if missing, set default of 0 and add to @request_params
+
   end
 
-
+  def format_validation_response
+    p(@error_messages)
+    p(@missing_params)
+    errors = @error_messages.any? || @missing_params.any?
+    if errors
+      p("There are errors")
+    else
+      p("There are no errors")
+    end
+    # {
+    #   errors: T/F,
+    #   messages: none if F
+    # }
+    # if there are errors: return {errors: t/f, messages: []}
+    # else return request params with nonstop included
+    # ErrorMessages::MISSING_PARAMS
+  end
 
 
 end
